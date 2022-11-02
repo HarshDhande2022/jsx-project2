d758127f10ae85d093a68c53135a97399299ceed
HarshDhande2022 <98374763+HarshDhande2022@users.noreply.github.com>
2022-11-02 19:58:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import img from './images/airbimg.png';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Evolution of</h1>
+      <h1>Airbnb's Frontend</h1>
+      <h5>@Spikebrehm</h5>  
+      <img src={img}/>
+    </div>
+    
+  );
+}
+
+export default App;
